50624d295b347555c7cfda4a4eaba6abb617154b
Fred Paucar <fred.paucarespinoza@gmail.com>
2020-03-10 12:54:16 -0500

    set init back

---
+++ demo/src/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const morgan = require('morgan');
+const app = express();
+
+app.set('port', process.env.PORT || 3000);
+
+app.use(morgan('dev'));
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(app.get('port'), () => {
+  console.log(`Server running on port ${app.get('port')}`);
+});
